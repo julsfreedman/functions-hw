1ca39239f72ae89468096d27d41f495dd1989c2b
Julia Freedman <113859369+julsfreedman@users.noreply.github.com>
2022-10-05 21:54:33 -0400

    maxOfThree (Question #2)
    W2D1 HW: Javascript Functions Question #2 maxOfThree

--- functions-hw.js
+++ functions-hw.js
@@ -1,9 +1,20 @@
 //1.
-function maxOfTwoNumbers(x, y) {
-  if (x >= y) return x;
-  else return y;
-}
+// function maxOfTwoNumbers(x, y) {
+//   if (x >= y) return x;
+//   else return y;
+// }
 
-console.log(maxOfTwoNumbers(10, 20));
+// console.log(maxOfTwoNumbers(10, 20));
 
 //terminal printed 20
+
+//2.
+const maxOfThree = function (x, y, z) {
+  if (x >= y && x >= z) return x;
+  if (y >= z) return y;
+  else return z;
+};
+
+console.log(maxOfThree(2, 4, 6));
+
+//terminal printed 6 for (2, 4, 6) and also worked for all other tested arguments
