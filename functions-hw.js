9961e78dbb3f272e5a7936c2d9f7ad49959521a6
Julia Freedman <113859369+julsfreedman@users.noreply.github.com>
2022-10-06 20:33:05 -0400

    Question #3
    W2D1 HW: Javascript Functions Answer to Question #3

--- functions-hw.js
+++ functions-hw.js
@@ -8,13 +8,37 @@
 
 //terminal printed 20
 
-//2.
-const maxOfThree = function (x, y, z) {
-  if (x >= y && x >= z) return x;
-  if (y >= z) return y;
-  else return z;
-};
+// //2.
+// const maxOfThree = function (x, y, z) {
+//   if (x >= y && x >= z) return x;
+//   if (y >= z) return y;
+//   else return z;
+// };
 
-console.log(maxOfThree(2, 4, 6));
+// console.log(maxOfThree(2, 4, 6));
 
-//terminal printed 6 for (2, 4, 6) and also worked for all other tested arguments
+// //terminal printed 6 for (2, 4, 6) and also worked for all other tested arguments
+
+//3.
+// function isCharAVowel(x) {
+//   if (x === "a" || x === "e" || x === "i" || x === "o" || x === "u") {
+//     return "true";
+//   }
+//   else {
+//     return "false";
+// }
+// console.log(isCharAVowel('a'))
+
+function isCharAVowel(char) {
+  return (
+    char === "a" ||
+    char === "e" ||
+    char === "i" ||
+    char === "o" ||
+    char === "u" ||
+    false
+  );
+}
+console.log(isCharAVowel("b"));
+//  printed false  for argument  "b" and printed true for argument  "a"
+// go over this one #3 in office hours (why doesn't my first answer work?)
